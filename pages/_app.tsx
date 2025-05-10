ea7eab3e29f1f0098caa1ef3c78ac250864d80f3
TauqeerTaj1122 <tauqeertaj91@gmail.com>
2025-05-10 15:04:01 +0500

    loading bar has been set on page change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { SessionProvider } from "next-auth/react";
 import "react-toastify/dist/ReactToastify.css";
@@ -10,15 +11,37 @@ import Header from "@/components/header";
 import Topbar from "@/components/topbar";
 import Footer from "@/components/footer";
 import StoreProvider from "./StoreProvider";
+import LoadingComponent from "@/components/LoadingBar";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  const { pathname } = router;
-  console.log("path:", pathname);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = () => {
+      setLoading(true);
+    };
+
+    const handleComplete = () => {
+      setLoading(false);
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, []);
+
   return (
     <>
       <SessionProvider session={pageProps.session}>
         <StoreProvider>
+          {loading && <LoadingComponent />}
           <Topbar />
           <Header />
           <Component {...pageProps} />
